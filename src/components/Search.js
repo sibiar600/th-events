635aaaccf2513f0f60530e6ba35b267ef67c9155
nonoumasy <nonoumasy@gmail.com>
2020-08-08 17:24:09 -0700

    asdfsaf

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,8 +4,8 @@ const Search = () => {
     return (
         <>
             <ul className='mr-10'>
-                <button className='bg-gray-200 text-xs text-gray-600 uppercase tracking-widest hover:bg-gray-300 font-bold px-6 py-3 rounded mr-3 text-teal-500 hover:bg-teal-500 hover:text-white'>Free</button>
-                <button className='bg-gray-200 text-xs text-gray-600 uppercase tracking-widest hover:bg-gray-300 font-bold px-6 py-3 rounded w-64 text-left'>Search</button>
+                <button className='bg-gray-200 text-xs text-gray-600 uppercase tracking-widest hover:bg-gray-300 font-bold px-6 py-3 rounded mr-3 text-teal-500 hover:bg-teal-500 hover:text-white focus:outline-none'>Free</button>
+                <button className='bg-gray-200 text-xs text-gray-600 uppercase tracking-widest hover:bg-gray-300 font-bold px-6 py-3 rounded w-64 text-left focus:outline-none'>Search</button>
             </ul>
         </>
     )

00f00ac6d6765ec878539fcef323112161a20b49
nonoumasy <nonoumasy@gmail.com>
2020-08-09 15:57:30 -0700

    sdfsdf

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -4,7 +4,7 @@ const Jumbotron = () => {
     return (
         <>
             <div className='w-screen mx-0'>
-                <img src="https://source.unsplash.com/1600x900/?tokyo" alt="" />
+                <img src="https://cors-anywhere.herokuapp.com/source.unsplash.com/1600x900/?tokyo" alt="" />
             </div>
 
         </>

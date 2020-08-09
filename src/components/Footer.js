8855d8f5f0372fc66d98645d92b8e80b7e77a164
nonoumasy <nonoumasy@gmail.com>
2020-08-09 05:10:19 -0700

    afsdf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,9 +9,9 @@ const Footer = () => {
             <div className='md:w-3/4 sm:w-4/5 mx-auto'>
                 <div className='pt-64 bg-gray-900'>
                     <ul className='bg-gray-900 ml-10'>
-                        <li className='text-semibold text-gray-400 text-3xl -m-2'>Designed by Nono Umasy</li>
-                        <li className='text-semibold text-gray-400 text-3xl -m-2'>Los Angeles, Tokyo, Lisbon</li>
-                        <li className='text-semibold text-gray-400 text-3xl -m-2'>hello@tokyohoods</li>
+                        <li className='text-semibold text-gray-400 text-3xl -m-2 xs:text-sm'>Designed by Nono Umasy</li>
+                        <li className='text-semibold text-gray-400 text-3xl -m-2 xs:text-sm'>Los Angeles, Tokyo, Lisbon</li>
+                        <li className='text-semibold text-gray-400 text-3xl -m-2 xs:text-sm'>hello@tokyohoods</li>
                     </ul>
 
                     <div className='pt-12'>

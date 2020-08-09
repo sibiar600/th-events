df07b11020bb1aebcd5de164a56bb6cc4ef8ae24
nonoumasy <nonoumasy@gmail.com>
2020-08-08 19:38:44 -0700

    sdfasdf

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React from 'react'
 
+
+
 const Footer = () => {
+
     return (
         <>
             <div className='h-64 bg-gray-200'></div>
@@ -8,7 +11,7 @@ const Footer = () => {
                 <div className='md:w-3/4 sm:w-4/5 mx-auto'>
                     <div className='flex justify-between'>
                         <div className='text-xs text-gray-600 mb-4 ml-10'>©2020 TokyoHoods</div>
-                        <div className='text-xs text-gray-600 mb-4 mr-10'>Back to the Top</div>
+                        <a href='/' className='text-xs text-gray-600 mb-4 mr-10'>Back to the Top</a>
                     </div>
 
                 </div>

3e058c7dd1e79db89088868096611a34d638e9e3
nonoumasy <nonoumasy@gmail.com>
2020-08-09 03:27:47 -0700

    adsfsf

--- src/components/Card.js
+++ src/components/Card.js
@@ -14,9 +14,9 @@ const Card = ({ image, name, date, description, time, venue, city, price }) => {
                         <div className='font-bold text-sm text-gray-800 font-serif text-center'>{date}</div>
                     </a>
 
-                    <div className='mb-0 mt-6 font-light text-justify text-sm md:h-24 sm:h-24'>{description}</div>
+                    <div className='mt-10 font-light text-justify text-sm md:h-32 sm:h-24'>{description}</div>
 
-                    <ul className='md:mt-0 sm:mt-0'>
+                    <ul className='md:mt-12 sm:mt-0'>
                         <li className='text-sm'>{time}</li>
                         <li><a className='text-sm text-teal-500 font-bold' href='/'>{venue}</a>, <a className='text-sm' href='/'>{city}</a></li>
                         <li className='font-semibold text-sm'>{price}</li>

dcb3bf28abb304ee58f0f4d249b482c92b8152f5
nonoumasy <nonoumasy@gmail.com>
2020-08-08 17:15:14 -0700

    adf

--- src/components/Title.js
+++ src/components/Title.js
@@ -4,7 +4,7 @@ import Search from './Search'
 const Title = () => {
     return (
         <>
-            <div className='flex md:justify-between mt-12 flex-wrap'>
+            <div className='flex md:justify-between mt-24 flex-wrap'>
                 <h1 className='text-3xl text-teal-500 font-serif leading-none ml-10'>Tokyo Events Calendar</h1>
                 <Search />
             </div>
